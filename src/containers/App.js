aef3c0478c97a653084c5b0373a80629950b584a
CodeAgony <yaroslav.bkh@gmail.com>
2019-06-15 18:41:00 +0300

    Configure CameraSelector to lift state up

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,10 +1,16 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
+import CameraSelector from '../components/CameraSelector/CameraSelector'
 
-function App() {
+function App() { 
+  const [camera, setCamera] = useState("all"); //Default API value
+  const passCamera = (event) => {
+    setCamera(event.target.value);
+  }
   return (
     <div className="App">
-     <h1>Les do dis</h1>
+      <h1>Les do dis</h1>
+      <CameraSelector onChange={passCamera} camera={camera}/>
     </div>
   );
 }
