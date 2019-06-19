823f4ba82677bb0fc04fb85bdb85666489d001fb
CodeAgony <yaroslav.bkh@gmail.com>
2019-06-19 07:41:59 +0300

    Freezing it until I actually learn it

--- client/src/containers/App.js
+++ client/src/containers/App.js
@@ -1,28 +1,29 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import './App.css';
 import CameraSelector from '../components/CameraSelector/CameraSelector';
 
 function App() { 
   const [camera, setCamera] = useState([{ camera : "all" }]); //Default API value
+  const [users, setUsers] = useState([{ users : ''}]);
   const passCamera = (event) => {
     setCamera(event.target.value);
   }
   
-  const getData = () => {
-    axios.get('http://localhost:5000/lol')
+  useEffect(() => {
+    const fetchUsers = async () => axios.get('http://localhost:5000/')
     .then(res => {
-      let users = res.data;
-      setCamera(users);
-    })
-  }
-
-  getData();
+      setUsers(res.data);
+    });
+    fetchUsers();
+  }, []);
 
   return (
     <div className="App">
       <h1>Les do dis</h1>
       <CameraSelector handleChange={passCamera}/>
+      <div>{users.map((users, index) => <p key={index}>{users.index}</p>)}</div>
+      {/* <button onClick={fetchUsers}>Get users</button> */}
     </div>
   );
 }
