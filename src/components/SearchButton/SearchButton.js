c04839ada77e8eb192f4605d4e1123ecdd5f34ec
CodeAgony <yaroslav.bkh@gmail.com>
2019-09-15 18:56:57 +0300

    Add a suicidal alert

--- src/components/SearchButton/SearchButton.js
+++ src/components/SearchButton/SearchButton.js
@@ -1,15 +1,24 @@
 import React, { useContext } from 'react';
+import Alert from '../Alert/Alert';
 import QueryContext from '../../context/query/queryContext';
+import AlertContext from '../../context/alert/alertContext';
 
 const SearchButton = () => {
   const queryContext = useContext(QueryContext);
-  const { getPhotos } = queryContext;
+  const { getPhotos, sol, date } = queryContext;
+  const alertContext = useContext(AlertContext);
+  const { alert, setAlert } = alertContext;
 
-  const onClick = () => getPhotos();
+  const onClick = () =>
+    sol === null && date === null ? setAlert(true) : getPhotos();
+
   return (
-    <button type="button" onClick={onClick}>
-      Search
-    </button>
+    <>
+      <button type="button" onClick={onClick}>
+        Search
+      </button>
+      {alert && <Alert />}
+    </>
   );
 };
 
