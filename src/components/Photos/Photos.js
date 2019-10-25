20ab53d65fda9d68c4b046e1d6c0b69360098fb4
CodeAgony <yaroslav.bkh@gmail.com>
2019-10-25 11:21:33 +0300

    Use auto-fill grid for photo cards

--- src/components/Photos/Photos.js
+++ src/components/Photos/Photos.js
@@ -1,13 +1,15 @@
 import React, { useContext } from 'react';
 import QueryContext from '../../context/query/queryContext';
 import PhotoCard from './PhotoCard';
+import styles from './Photos.module.css';
 
 const Photos = () => {
   const queryContext = useContext(QueryContext);
+  const { container } = styles;
   const { photos } = queryContext;
 
   return (
-    <div>
+    <div className={container}>
       {photos &&
         photos.length > 0 &&
         photos.map(photo => <PhotoCard key={photo.id} photo={photo} />)}
