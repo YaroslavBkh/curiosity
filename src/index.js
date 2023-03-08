db4a01fdaf4ac9b0c50e3eef81c21ebd03191a27
Yaroslav <yaroslav.bkh@gmail.com>
2023-03-08 10:33:53 +0200

    chore: audit fix, nuke cra comments and workers

--- src/index.js
+++ src/index.js
@@ -4,7 +4,6 @@ import './index.css';
 import QueryState from './context/query/QueryState';
 import AlertState from './context/alert/AlertState';
 import App from './containers/App';
-import * as serviceWorker from './serviceWorker';
 
 const root = document.getElementById('root');
 ReactDOM.render(
@@ -15,8 +14,3 @@ ReactDOM.render(
   </QueryState>,
   root
 );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
